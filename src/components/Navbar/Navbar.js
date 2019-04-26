7b3a1139fbc427005aa2d9b54234913c442ebd60
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-04-26 16:41:57 -0500

    navbar css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,14 @@ export default class Navbars extends Component {
   render() {
     return (
       <div className="navbar">
-        <div className>Navbar</div>
+        <div className="nav-left-side">
+          <p className="nav-brands">Brands</p>
+          <p className="nav-influencer">I'm an influencer</p>
+        </div>
+        <div className="nav-right-side">
+          <button className="nav-login">Login</button>
+          <button className="nav-sign-up">Sign up for free</button>
+        </div>
       </div>
     );
   }
