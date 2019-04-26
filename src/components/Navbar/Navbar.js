15686b16e4a64d6ec51895de0e29cd073121e6c3
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-04-26 15:18:10 -0500

    changed sass to scss

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Navbar.scss";
+
+export default class Navbars extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <div className>Navbar</div>
+      </div>
+    );
+  }
+}
