a5cbe382725358eaa6228d4ee196ed265d540dc1
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-05-01 11:23:11 -0500

    displayInfluencers

--- src/components/displayInfluencers/displayInfluencers.js
+++ src/components/displayInfluencers/displayInfluencers.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class displayInfluencers extends Component {
+export default class DisplayInfluencers extends Component {
   constructor(props) {
     super(props);
   }
