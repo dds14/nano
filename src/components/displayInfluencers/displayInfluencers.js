41ce713347e91ecfb7b62d868b52b66e06e0c36a
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-05-03 10:23:24 -0500

    css to display influencers

--- src/components/displayInfluencers/displayInfluencers.js
+++ src/components/displayInfluencers/displayInfluencers.js
@@ -21,19 +21,39 @@ export default class DisplayInfluencers extends Component {
 
   render() {
     return (
-      <div className="whole-div">
-        {this.state.influencerProfiles.map((val, index) => {
-          return (
-            <div className="overall-account-display">
-              <div className="home-account-name">{val.igaccountname}</div>
-              <img
-                src={val.profilepicture}
-                alt="influencer-profile-picture"
-                className="home-profile-picture"
-              />
-            </div>
-          );
-        })}
+      <div className="display-influencers">
+        <h1 className="home-influencers-title">Our Influencers</h1>
+        <div className="whole-div">
+          {this.state.influencerProfiles.map((val, index) => {
+            return (
+              <div className="overall-account-display">
+                <div className="home-account-name">{val.igaccountname}</div>
+                <img
+                  src={val.profilepicture}
+                  alt="influencer-profile-picture"
+                  className="home-profile-picture"
+                />
+                <div className="home-account-atname">
+                  {"Username: " + val.atname}
+                </div>
+                <div className="home-follower-count">
+                  {"Followers: " + val.followercount}
+                </div>
+                <div className="home-average-likes">
+                  {"Average Likes: " + val.averagelikes}
+                </div>
+                <div className="ello">
+                  <div className="home-contact-influencer">
+                    <button className="home-contact-influencer-button">
+                      {" "}
+                      Contact
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
